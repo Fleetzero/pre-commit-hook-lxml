68a65bfeb8b35d828e164f5ccf53a0bc7ad91135
Felix Collin <felix@fleetzero.com>
2025-02-21 18:00:50 -0500

    fixed formatting with pre-commit hook! (max column check broken!!)

--- pre_commit_hooks/lxml_format.py
+++ pre_commit_hooks/lxml_format.py
@@ -12,12 +12,12 @@ from editorconfig import get_properties, EditorConfigError
 
 INDENT = 2
 RETRIES = 5
-ENV_PREFIX = "PRE_COMMIT_HOOK_LXML_FORMAT_"
+ENV_PREFIX = 'PRE_COMMIT_HOOK_LXML_FORMAT_'
 
 
 def pretty_print(
     content: bytes,
-    space: str = " ",
+    space: str = ' ',
     indent: int = INDENT,
     declaration: bool = True,
     sort: bool = False,
@@ -37,7 +37,8 @@ def pretty_print(
     Returns:
       bytes: The pretty printed XML content.
     """
-    parser = etree.XMLParser(remove_blank_text=False, recover=True, strip_cdata=False)
+    parser = etree.XMLParser(remove_blank_text=False,
+                             recover=True, strip_cdata=False)
     tree = etree.XML(content, parser=parser)
     root_tree = tree.getroottree()
     etree.indent(root_tree, space=space * indent)
@@ -46,7 +47,8 @@ def pretty_print(
             if elem.attrib:  # Only sort if there are attributes
                 sorted_attrs = sorted(elem.attrib.items())  # Efficient sorting
                 elem.attrib.clear()
-                elem.attrib.update(sorted_attrs)  # Update in-place (avoids extra dict)
+                # Update in-place (avoids extra dict)
+                elem.attrib.update(sorted_attrs)
     return etree.tostring(
         root_tree,
         pretty_print=True,
@@ -68,17 +70,19 @@ def get_indent_from_editorconfig(filename: str) -> tuple[int, str]:
     """
     try:
         properties = get_properties(os.path.abspath(filename))
-        if "indent_style" in properties:
-            style = properties["indent_style"]
-            if style == "tab":
-                return 1, "\t"
-            if "indent_size" in properties and style == "space":
-                return int(properties["indent_size"]), " "
+        if 'indent_style' in properties:
+            style = properties['indent_style']
+            if style == 'tab':
+                return 1, '\t'
+            if 'indent_size' in properties and style == 'space':
+                return int(properties['indent_size']), ' '
     except EditorConfigError:
-        logging.warning("Error getting EditorConfig properties.", exc_info=True)
+        logging.warning(
+            'Error getting EditorConfig properties.', exc_info=True)
     except ValueError:
-        logging.warning("Error parsing indent_size in editorconfig.", exc_info=True)
-    return INDENT, " "
+        logging.warning(
+            'Error parsing indent_size in editorconfig.', exc_info=True)
+    return INDENT, ' '
 
 
 def beautify(
@@ -87,9 +91,9 @@ def beautify(
     retries: int = RETRIES,
     write: bool = False,
     sort: bool = False,
-    endings: str = "auto",
-    self_closing: str = "auto",
-    declaration: str = "auto",
+    endings: str = 'auto',
+    self_closing: str = 'auto',
+    declaration: str = 'auto',
 ) -> bool:
     """
     Beautifies, e.g. gently reformat the XML content of a file. Changes can be
@@ -115,19 +119,19 @@ def beautify(
             f"Indentation set to {indent} spaces via editorconfig or default."
         )
     else:
-        space = " "
+        space = ' '
         logging.debug(f"Indentation set to {indent} via CLI")
 
     # Read file content, binary mode into original variable
     try:
-        with open(filename, "rb") as f:
+        with open(filename, 'rb') as f:
             original = f.read()
     except Exception as e:
         logging.error(f"Failed to read file: {filename}: {e}")
         return False
 
     # Detect if we should output an XML declaration in the first place.
-    absence = re.compile(r"^\s*(false|off|no|0|f|n)\s*$", re.IGNORECASE)
+    absence = re.compile(r'^\s*(false|off|no|0|f|n)\s*$', re.IGNORECASE)
     xml_declaration = not absence.match(declaration)
 
     # Pretty print the content until it has not changed between two iterations.
@@ -147,65 +151,71 @@ def beautify(
     # Fix self-closing tags if necessary, i.e. add an extra space before the '/>'
     # characters (LXML removes that space, if there was one). This is an
     # eye-candy, little more.
-    if self_closing == "space":
+    if self_closing == 'space':
         logging.debug(f"Self-closing tags will have a space: {filename}")
-        xml = xml.replace(b"/>\n", b" />\n")
-        xml = xml.replace(b"  />\n", b" />\n")
-    elif self_closing == "auto":
-        if b" />" in original:
-            logging.info(f"Detected self-closing tags with space in {filename}")
-            xml = xml.replace(b"/>\n", b" />\n")
-            xml = xml.replace(b"  />\n", b" />\n")
+        xml = xml.replace(b'/>\n', b' />\n')
+        xml = xml.replace(b'  />\n', b' />\n')
+    elif self_closing == 'auto':
+        if b' />' in original:
+            logging.info(
+                f"Detected self-closing tags with space in {filename}")
+            xml = xml.replace(b'/>\n', b' />\n')
+            xml = xml.replace(b'  />\n', b' />\n')
 
     # Fix XML declaration when relevant
     if xml_declaration:
         # XML declaration is always on the first line, so partition on the
         # first ending ?>
-        parts = xml.partition(b"?>")
-        if parts[0].startswith(b"<?xml"):
+        parts = xml.partition(b'?>')
+        if parts[0].startswith(b'<?xml'):
             # Detect quoting style in the rest of the XML file. This
             # is crude and prefers double-quoting.
-            if declaration == "auto":
+            if declaration == 'auto':
                 if parts[2].find(b'="') != -1:
-                    declaration = "double-quotes"
-                    logging.debug(f"Detected use of double quotes in {filename}")
+                    declaration = 'double-quotes'
+                    logging.debug(
+                        f"Detected use of double quotes in {filename}")
                 elif parts[2].find(b"='") != -1:
-                    declaration = "single-quotes"
-                    logging.debug(f"Detected use of single quotes in {filename}")
+                    declaration = 'single-quotes'
+                    logging.debug(
+                        f"Detected use of single quotes in {filename}")
                 else:
-                    declaration = "double-quotes"
+                    declaration = 'double-quotes'
                     logging.warning(
-                        f"Cannot detect quoting style in {filename}, defaulting to double quotes"
+                        f"Cannot detect quoting style in {
+                            filename}, defaulting to double quotes"
                     )
 
             # Reconstruct XML declaration with the requested/detected
             # quoting style.
-            if declaration == "double-quotes":
+            if declaration == 'double-quotes':
                 xml = parts[0].replace(b"'", b'"') + parts[1] + parts[2]
-                logging.debug(f"Double quotes enforced in declaration: {filename}")
-            elif declaration == "single-quotes":
+                logging.debug(
+                    f"Double quotes enforced in declaration: {filename}")
+            elif declaration == 'single-quotes':
                 xml = parts[0].replace(b'"', b"'") + parts[1] + parts[2]
-                logging.debug(f"Single quotes enforced in declaration: {filename}")
+                logging.debug(
+                    f"Single quotes enforced in declaration: {filename}")
         else:
             logging.error(f"Cannot find XML declaration in {filename}")
 
     # Convert line endings, if relevant. Detect from original content if
     # necessary. Note: the output of pretty_print is always using unix line
     # endings.
-    if endings == "windows":
+    if endings == 'windows':
         logging.debug(f"Windows line endings enforced: {filename}")
-        xml = xml.replace(b"\n", b"\r\n")
-    elif endings == "mac":
+        xml = xml.replace(b'\n', b'\r\n')
+    elif endings == 'mac':
         logging.debug(f"Mac (classic) line endings enforced: {filename}")
-        xml = xml.replace(b"\n", b"\r")
-    elif endings == "auto":
+        xml = xml.replace(b'\n', b'\r')
+    elif endings == 'auto':
         # windows or ancient mac somewhere?
-        if b"\r\n" in original:
+        if b'\r\n' in original:
             logging.info(f"Windows line endings detected: {filename}")
-            xml = xml.replace(b"\n", b"\r\n")
-        elif b"\r" in original and b"\r\n" not in original:
+            xml = xml.replace(b'\n', b'\r\n')
+        elif b'\r' in original and b'\r\n' not in original:
             logging.info(f"Mac (classic) line endings detected: {filename}")
-            xml = xml.replace(b"\n", b"\r")
+            xml = xml.replace(b'\n', b'\r')
 
     # Log/return the result and write the file if the write flag is set.
     if xml == content:
@@ -216,7 +226,7 @@ def beautify(
         if write:
             logging.info(f"Formatted: {filename}")
             try:
-                with open(filename, "wb") as f:
+                with open(filename, 'wb') as f:
                     f.write(xml)
             except Exception as e:
                 logging.error(f"Failed to write file: {filename}: {e}")
@@ -242,7 +252,7 @@ def str_to_bool(s) -> bool:
       bool: The boolean value corresponding to the string.
 
     """
-    pattern = re.compile(r"^\s*(true|on|yes|1|t|y)\s*$", re.IGNORECASE)
+    pattern = re.compile(r'^\s*(true|on|yes|1|t|y)\s*$', re.IGNORECASE)
     return bool(pattern.match(s))
 
 
@@ -264,92 +274,92 @@ def main(argv: Sequence[str] | None = None) -> int:
     """
     argv = argv if argv is not None else sys.argv[1:]
     parser = argparse.ArgumentParser(
-        prog="lxml_format", description="Prettyprint XML file with lxml"
+        prog='lxml_format', description='Prettyprint XML file with lxml'
     )
 
     parser.add_argument(
-        "-e",
-        "--endings",
-        "--line-endings",
-        dest="endings",
-        choices=["unix", "windows", "mac", "auto"],
-        default="auto",
-        help="Line endings in the formatted file. Default: %(default)s",
+        '-e',
+        '--endings',
+        '--line-endings',
+        dest='endings',
+        choices=['unix', 'windows', 'mac', 'auto'],
+        default='auto',
+        help='Line endings in the formatted file. Default: %(default)s',
     )
 
     parser.add_argument(
-        "-i",
-        "--indent",
-        dest="indent",
+        '-i',
+        '--indent',
+        dest='indent',
         type=int,
         default=-1,
-        help="Number of spaces to use, overrides .editorconfig when positive. Default: %(default)s",
+        help='Number of spaces to use, overrides .editorconfig when positive. Default: %(default)s',
     )
 
     parser.add_argument(
-        "-l",
-        "--log-level",
-        "--log",
-        choices=["DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"],
-        dest="loglevel",
-        default="INFO",
-        help="Debug level.",
+        '-l',
+        '--log-level',
+        '--log',
+        choices=['DEBUG', 'INFO', 'WARNING', 'ERROR', 'CRITICAL'],
+        dest='loglevel',
+        default='INFO',
+        help='Debug level.',
     )
 
     parser.add_argument(
-        "-r",
-        "--retries",
-        dest="retries",
+        '-r',
+        '--retries',
+        dest='retries',
         type=int,
         default=RETRIES,
-        help="Max number of retries to reach content stabilisation. Default: %(default)s",
+        help='Max number of retries to reach content stabilisation. Default: %(default)s',
     )
 
     parser.add_argument(
-        "-s",
-        "--self-closing",
-        dest="self_closing",
-        choices=["space", "nospace", "auto"],
-        default="space",
-        help="Should self-closing tags have an ending space? Default: %(default)s",
+        '-s',
+        '--self-closing',
+        dest='self_closing',
+        choices=['space', 'nospace', 'auto'],
+        default='space',
+        help='Should self-closing tags have an ending space? Default: %(default)s',
     )
 
     parser.add_argument(
-        "-w",
-        "--write",
-        action="store_true",
-        dest="write",
-        help="Write the changes back to the file",
+        '-w',
+        '--write',
+        action='store_true',
+        dest='write',
+        help='Write the changes back to the file',
     )
 
     parser.add_argument(
-        "-t",
-        "--sort",
-        action="store_true",
-        dest="sort",
-        help="Sort the file alphabetically",
+        '-t',
+        '--sort',
+        action='store_true',
+        dest='sort',
+        help='Sort the file alphabetically',
     )
 
     parser.add_argument(
-        "-d",
-        "--declaration",
+        '-d',
+        '--declaration',
         choices=[
-            "auto",
-            "no",
-            "off",
-            "false",
-            "0",
-            "f",
-            "n",
-            "single-quotes",
-            "double-quotes",
+            'auto',
+            'no',
+            'off',
+            'false',
+            '0',
+            'f',
+            'n',
+            'single-quotes',
+            'double-quotes',
         ],
-        default="auto",
-        dest="declaration",
-        help="Should we have an XML declaration and what quoting-style should it use? Default: %(default)s",
+        default='auto',
+        dest='declaration',
+        help='Should we have an XML declaration and what quoting-style should it use? Default: %(default)s',
     )
 
-    parser.add_argument("filenames", nargs="*", help="Files to format")
+    parser.add_argument('filenames', nargs='*', help='Files to format')
 
     args = parser.parse_args(argv)
 
@@ -359,42 +369,45 @@ def main(argv: Sequence[str] | None = None) -> int:
     indent: int = int(os.environ.get(f"{ENV_PREFIX}INDENT", args.indent))
     retries: int = int(os.environ.get(f"{ENV_PREFIX}RETRIES", args.retries))
     loglevel = os.environ.get(f"{ENV_PREFIX}LOG_LEVEL", args.loglevel)
-    write: bool = str_to_bool(os.environ.get(f"{ENV_PREFIX}WRITE", str(args.write)))
-    sort: bool = str_to_bool(os.environ.get(f"{ENV_PREFIX}SORT", str(args.sort)))
+    write: bool = str_to_bool(os.environ.get(
+        f"{ENV_PREFIX}WRITE", str(args.write)))
+    sort: bool = str_to_bool(os.environ.get(
+        f"{ENV_PREFIX}SORT", str(args.sort)))
     endings = os.environ.get(f"{ENV_PREFIX}LINE_ENDINGS", args.endings).lower()
     self_closing = os.environ.get(
         f"{ENV_PREFIX}SELF_CLOSING", args.self_closing
     ).lower()
-    declaration = os.environ.get(f"{ENV_PREFIX}DECLARATION", args.declaration).lower()
+    declaration = os.environ.get(
+        f"{ENV_PREFIX}DECLARATION", args.declaration).lower()
 
     # Setup logging
     numeric_level = getattr(logging, loglevel.upper(), None)
     if not isinstance(numeric_level, int):
-        raise ValueError("Invalid log level: %s" % loglevel)
+        raise ValueError('Invalid log level: %s' % loglevel)
     logging.basicConfig(
         level=numeric_level,
-        format="[lxml_format] [%(asctime)s.%(msecs)03d] [%(levelname)s] %(message)s",
-        datefmt="%Y%m%d %H%M%S",
+        format='[lxml_format] [%(asctime)s.%(msecs)03d] [%(levelname)s] %(message)s',
+        datefmt='%Y%m%d %H%M%S',
     )
 
     # Check line endings and self-closing mode (not only CLI arguments, but
     # also environment variables)
-    if endings not in ["unix", "windows", "mac", "auto"]:
+    if endings not in ['unix', 'windows', 'mac', 'auto']:
         logging.error(f"Invalid line endings: {endings}")
         return 1
-    if self_closing not in ["space", "nospace", "auto"]:
+    if self_closing not in ['space', 'nospace', 'auto']:
         logging.error(f"Invalid self-closing tag mode: {self_closing}")
         return 1
     if declaration not in [
-        "auto",
-        "no",
-        "off",
-        "false",
-        "0",
-        "f",
-        "n",
-        "single-quotes",
-        "double-quotes",
+        'auto',
+        'no',
+        'off',
+        'false',
+        '0',
+        'f',
+        'n',
+        'single-quotes',
+        'double-quotes',
     ]:
         logging.error(f"Invalid XML declaration mode: {declaration}")
         return 1
@@ -428,5 +441,5 @@ def main(argv: Sequence[str] | None = None) -> int:
         return 1
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     raise SystemExit(main())
